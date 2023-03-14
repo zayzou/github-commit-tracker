5f2b2229a3be12c446ac8042aa8c4b959d205fa2
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-14 21:30:08 +0100

    Implement TelegramService

--- src/main/java/com/zayzou/service/TelegramService.java
+++ src/main/java/com/zayzou/service/TelegramService.java
@@ -1,11 +1,21 @@
 package com.zayzou.service;
 
+import com.zayzou.utils.OkHttpUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TelegramService {
 
-    public void send() {
-        System.out.println("send from service");
+    private final OkHttpUtils okHttpUtils;
+
+    public TelegramService(OkHttpUtils okHttpUtils) {
+        this.okHttpUtils = okHttpUtils;
+    }
+
+    public void send(String message) {
+        String user_id = "5183089051";
+        this.okHttpUtils.httpCall(
+                "https://api.telegram.org/bot6089177075:AAEPBF7F4RqJOe_crCXccIvENc9JuYxBOBU/sendMessage?chat_id=" + user_id + "&text=" + message,
+                "POST");
     }
 }
