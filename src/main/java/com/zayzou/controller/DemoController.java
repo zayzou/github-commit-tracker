e658545f4117715a866cf49063cc0799cec173a5
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-14 21:43:03 +0100

    update .gitignore

--- src/main/java/com/zayzou/controller/DemoController.java
+++ src/main/java/com/zayzou/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.zayzou.controller;
 
 import com.zayzou.GithubProperties;
 import com.zayzou.service.EmailService;
+import com.zayzou.service.TelegramService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +14,27 @@ public class DemoController {
 
     private final GithubProperties githubProperties;
     private final EmailService emailService;
+    private final TelegramService telegramService;
 
-    public DemoController(GithubProperties githubProperties, EmailService emailService) {
+    public DemoController(GithubProperties githubProperties, EmailService emailService, TelegramService telegramService) {
         this.githubProperties = githubProperties;
         this.emailService = emailService;
+        this.telegramService = telegramService;
     }
 
+    @GetMapping(params = "telegram")
+    public String telegram() {
+        telegramService.send("message");
+        return "telegram message sent";
+    }
 
-    @GetMapping
-    public String demo() {
-        emailService.send();
+    @GetMapping(params = "email")
+    public String email() {
+        emailService.send("text");
         return "done";
 
     }
+
+
+
 }
