d167dd0f0e50d710acbea18f285f96b25557d3d4
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-14 21:31:57 +0100

    Implement GithubService

--- src/main/java/com/zayzou/service/GithubService.java
+++ src/main/java/com/zayzou/service/GithubService.java
@@ -2,7 +2,12 @@ package com.zayzou.service;
 
 import com.zayzou.utils.OkHttpUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 @Slf4j
@@ -14,7 +19,22 @@ public class GithubService {
         this.okHttpUtils = okHttpUtils;
     }
 
+
+    private String getContributionForDate(String string, String date) {
+        Document doc = Jsoup.parse(string);
+        return doc.getElementsByAttributeValue("data-date", date).first().ownText();
+    }
+
+    private String currentDate() {
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+        return now.format(formatter);
+    }
+
     public String getUpdates() {
-        return this.okHttpUtils.get();
+        var responseValue = this.okHttpUtils.httpCall(
+                "https://github.com/users/Soffi-Zahir/contributions?to=2023-01-01",
+                "GET");
+        return getContributionForDate(responseValue, currentDate());
     }
 }
