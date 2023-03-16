ed4a61da2887949de907956cee07aa4ad5215d38
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-16 19:19:22 +0100

    register telegrambot api

--- src/main/java/com/zayzou/GithubCommitTrackerApplication.java
+++ src/main/java/com/zayzou/GithubCommitTrackerApplication.java
@@ -1,8 +1,12 @@
 package com.zayzou;
 
+import com.zayzou.controller.BotController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +14,12 @@ public class GithubCommitTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubCommitTrackerApplication.class, args);
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new BotController());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
 }
