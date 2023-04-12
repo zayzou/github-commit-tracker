584a092b8fe46ea8b3b9f607ad43cb55cf732338
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-04-12 23:26:31 +0100

    add copyright

--- src/main/java/com/zayzou/GithubCommitTrackerApplication.java
+++ src/main/java/com/zayzou/GithubCommitTrackerApplication.java
@@ -1,8 +1,16 @@
+/*
+ * Copyright (C) 2023
+ *
+ * Author : z.soffi
+ * File : GithubCommitTrackerApplication.java
+ */
+
 package com.zayzou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
 @EnableScheduling
 public class GithubCommitTrackerApplication {
