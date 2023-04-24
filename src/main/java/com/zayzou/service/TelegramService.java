f796920d72b8fbfea7262abd614978775c42397e
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-04-24 13:34:51 +0100

    add method that return contributions

--- src/main/java/com/zayzou/service/TelegramService.java
+++ src/main/java/com/zayzou/service/TelegramService.java
@@ -30,11 +30,20 @@ public class TelegramService {
     }
 
     public void send(String command) {
-        String message = Objects.equals(command, "today") ? githubService.getTodayContribution() : githubService.getCurrentYearContribution(); // Fetch Github contributions based on command ("today" or "year")
-        String reaction = message.startsWith("No") ? "😭" : "🥳"; // Set reaction emoji based on Github contributions message
-        message = String.format("%s %s", reaction, message); // Format the final message with reaction emoji
+        String message = getMessage(command);
         this.okHttpUtils.httpCall(
                 "https://api.telegram.org/bot" + botId + "/sendMessage?chat_id=" + userId + "&text=" + message, // Construct Telegram API URL with bot ID, user ID, and message
                 "POST"); // Make HTTP POST call to send the message to Telegram
+    }
+
+    public String sendAndReturn(String command) {
+        return getMessage(command);
+    }
+
+    private String getMessage(String command) {
+        String message = Objects.equals(command, "today") ? githubService.getTodayContribution() : githubService.getCurrentYearContribution(); // Fetch Github contributions based on command ("today" or "year")
+        String reaction = message.startsWith("No") ? "😭" : "🥳"; // Set reaction emoji based on Github contributions message
+        message = String.format("%s %s", reaction, message); // Format the final message with reaction emoji
+        return message;
     }
 }
