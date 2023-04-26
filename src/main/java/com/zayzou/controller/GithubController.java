c7a4966605a6739832ed7d2e6818b043341356f3
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-04-26 20:12:34 +0100

    add cross origin to controller

--- src/main/java/com/zayzou/controller/GithubController.java
+++ src/main/java/com/zayzou/controller/GithubController.java
@@ -10,6 +10,7 @@ package com.zayzou.controller;
 import com.zayzou.service.GithubService;
 import com.zayzou.service.TelegramService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping(value = "github", produces = "application/json")
-// @CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:8080")
 public class GithubController {
 
     GithubService githubService;
