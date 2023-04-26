7367b1486be1b4ea7181e91358594047b330409f
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-04-26 14:59:02 +0100

    Make RootController a rest controller

--- src/main/java/com/zayzou/controller/RootController.java
+++ src/main/java/com/zayzou/controller/RootController.java
@@ -8,5 +8,14 @@
 package com.zayzou.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
 public class RootController {
+
+
 }
