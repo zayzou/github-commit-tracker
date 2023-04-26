5430b95507d24908ff942a5e2317a41b58193552
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-04-26 17:20:54 +0100

    implement execute command

--- src/main/java/com/zayzou/controller/RootController.java
+++ src/main/java/com/zayzou/controller/RootController.java
@@ -8,14 +8,33 @@
 package com.zayzou.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.zayzou.service.GithubService;
+import com.zayzou.service.TelegramService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1", produces = "application/json")
 @CrossOrigin(origins = "http://localhost:8080")
 public class RootController {
+    private TelegramService telegramService;
+    private GithubService githubService;
 
+    public RootController(TelegramService telegramService, GithubService githubService) {
+        this.telegramService = telegramService;
+        this.githubService = githubService;
+    }
 
+    @GetMapping("{command}")
+    public String execute(@PathVariable String command) {
+        String updates = "";
+        if (command.equals("year")) {
+            updates = githubService.getCurrentYearContribution();
+        } else if (command.equals("today")) {
+            updates = githubService.getTodayContribution();
+        } else {
+            updates = "Invalid command :)";
+        }
+        return updates;
+
+    }
 }
