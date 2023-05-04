cb315f52e20d0cc70dedb798094aa690dc2a4a51
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-04 22:52:48 +0100

    implement generateToken

--- src/main/java/com/zayzou/security/JwtService.java
+++ src/main/java/com/zayzou/security/JwtService.java
@@ -9,11 +9,16 @@ package com.zayzou.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -23,6 +28,18 @@ public class JwtService {
 
     public String extractUsername(String jwt) {
         return extractClaim(jwt, Claims::getSubject);
+    }
+
+    public String generateToken(
+            Map<String, Objects> extractClaims,
+            UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
