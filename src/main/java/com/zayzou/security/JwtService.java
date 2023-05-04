8561a6ee3389e6b6adfd70f9a8e7d1f3023d8a30
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-04 23:05:18 +0100

    implement isTokenValid

--- src/main/java/com/zayzou/security/JwtService.java
+++ src/main/java/com/zayzou/security/JwtService.java
@@ -48,6 +48,19 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        var username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
         final Claims claims = extractAllClaims(token);
         return claimResolver.apply(claims);
