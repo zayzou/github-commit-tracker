f89ad5bebd48180c104675956f7c207a292927b6
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-04 19:41:39 +0100

    create JwtService

---
+++ src/main/java/com/zayzou/security/JwtService.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2023
+ *
+ * Author : z.soffi
+ * File : JwtService.java
+ */
+
+package com.zayzou.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String jwt) {
+        return "";
+    }
+}
