f2a8a1ad0e7e4d4ccf9833af77f06adda2f1e148
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-04 21:05:16 +0100

    implement getSignInKey

--- src/main/java/com/zayzou/security/JwtService.java
+++ src/main/java/com/zayzou/security/JwtService.java
@@ -9,7 +9,11 @@ package com.zayzou.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
@@ -27,5 +31,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJwt(jwt)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
