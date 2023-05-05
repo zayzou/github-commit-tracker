68362cb109cbd71b42de9e1ce2d8e0c3f4735fd1
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-05 12:32:23 +0100

    add userRepository

--- src/main/java/com/zayzou/security/SecurityConfig.java
+++ src/main/java/com/zayzou/security/SecurityConfig.java
@@ -10,21 +10,16 @@ package com.zayzou.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final UserRepository userRepository;
     @Bean
     public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return null;
-            }
+        return username -> userRepository.findByEmail(username).orElseThrow();
         }
     }
-}
+
