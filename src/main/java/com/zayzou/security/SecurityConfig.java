ddc1912573228eb931d4373caffb20cc7a269afb
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-05 12:29:49 +0100

    implement userDetailService

--- src/main/java/com/zayzou/security/SecurityConfig.java
+++ src/main/java/com/zayzou/security/SecurityConfig.java
@@ -8,9 +8,23 @@
 package com.zayzou.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return null;
+            }
+        }
+    }
 }
