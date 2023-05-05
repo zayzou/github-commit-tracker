e461f2a9f756d19ec410d0094c7e3bead9f2331f
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-05 12:21:49 +0100

    create SecurityConfig

---
+++ src/main/java/com/zayzou/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023
+ *
+ * Author : z.soffi
+ * File : SecurityConfig.java
+ */
+
+package com.zayzou.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
