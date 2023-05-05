f9fe7c33c8d795aabc83135bd0636142f6fe128b
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-05 12:34:15 +0100

    add UsernameNotFoundException

--- src/main/java/com/zayzou/security/SecurityConfig.java
+++ src/main/java/com/zayzou/security/SecurityConfig.java
@@ -11,15 +11,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final UserRepository userRepository;
+
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> userRepository.findByEmail(username).orElseThrow();
-        }
+        return username -> userRepository
+                .findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found :("));
     }
+}
 
