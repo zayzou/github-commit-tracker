ce1a70fc853b6772d2ca5838cdc2f778076b8af6
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-07 17:24:28 +0100

    create GithubService for graphql calls

---
+++ src/main/java/com/zayzou/graphql/GithubService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2023
+ *
+ * Author : z.soffi
+ * File : GithubService.java
+ */
+
+package com.zayzou.graphql;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GithubService {
+
+}
