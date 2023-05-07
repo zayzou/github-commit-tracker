acb98ac922a36e3f1e83e118271413b59f295002
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-07 17:19:28 +0100

    create GithubGraphQL that will make graphql calls

---
+++ src/main/java/com/zayzou/graphql/GithubGraphQL.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2023
+ *
+ * Author : z.soffi
+ * File : Controller.java
+ */
+
+package com.zayzou.graphql;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GithubGraphQL {
+
+
+    private final String GITHUB_TOKEN = "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";
+}
