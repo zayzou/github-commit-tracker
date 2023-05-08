41de1a7e342e0b062541c50c0cc6f9b719fcec61
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-08 14:13:51 +0100

    write implementation of GithubGraphQLService

--- src/main/java/com/zayzou/graphql/GithubGraphQLService.java
+++ src/main/java/com/zayzou/graphql/GithubGraphQLService.java
@@ -7,9 +7,44 @@
 
 package com.zayzou.graphql;
 
+import org.springframework.graphql.client.HttpGraphQlClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class GithubGraphQLService {
+
+
+    private final HttpGraphQlClient graphQlClient;
+
+    public GithubGraphQLService() {
+        WebClient client = WebClient.builder()
+                .baseUrl("https://api.github.com/graphql")
+                .defaultHeader("Authorization", "Bearer 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")
+                .defaultHeader("content-type", "application/json")
+                .build();
+
+        graphQlClient = HttpGraphQlClient.builder(client).build();
+    }
+
+
+    public Mono<User> getUser() {
+        //language=GraphQL
+        String document = """
+                query {
+                   user(login: "zayzou") {
+                     name
+                     login
+                     avatarUrl
+                     contributionsCollection {
+                           years: contributionYears
+                         }
+                   }
+                 }
+                           """;
+        Mono<User> viewers = graphQlClient.document(document).retrieve("user").toEntity(User.class);
+        return viewers;
+    }
 
 }
