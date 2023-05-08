4a267393b8c185b27355d0b9f35461c5ec59e8bf
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-08 14:13:26 +0100

    write the basic code of GithubGraphQL API call

--- src/main/java/com/zayzou/graphql/GithubGraphQL.java
+++ src/main/java/com/zayzou/graphql/GithubGraphQL.java
@@ -7,11 +7,27 @@
 
 package com.zayzou.graphql;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
+@RequestMapping("graphql")
 public class GithubGraphQL {
 
 
     private final String GITHUB_TOKEN = "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";
+    private GithubGraphQLService githubGraphQLService;
+
+    public GithubGraphQL(GithubGraphQLService githubGraphQLService) {
+        this.githubGraphQLService = githubGraphQLService;
+    }
+
+    @GetMapping("/ping")
+    public void ping() {
+        Mono<User> user = this.githubGraphQLService.getUser();
+        user.subscribe(response -> System.out.println(response));
+    }
+
 }
