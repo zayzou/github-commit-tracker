5cd2a35a0c129bb05ee1d967ed94d4030c618eca
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-10 20:44:18 +0100

    implement returning json response in controller

--- src/main/java/com/zayzou/graphql/GithubGraphQL.java
+++ src/main/java/com/zayzou/graphql/GithubGraphQL.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,7 +21,7 @@ public class GithubGraphQL {
 
 
     private final GithubGraphQLService githubGraphQLService;
-    private List<Week> weeks = new ArrayList<>();
+
 
     public GithubGraphQL(GithubGraphQLService githubGraphQLService) {
         this.githubGraphQLService = githubGraphQLService;
@@ -34,22 +33,21 @@ public class GithubGraphQL {
         user.subscribe(response -> System.out.println(response));
     }
 
-    //        System.out.println("Total contribution : " + response.getContributionsCollection().getContributionCalendar().getTotal());
-    //week -> week.getDays().forEach(day -> System.out.println(day.getDate() + " : " + day.getContributionCount())
+
     @GetMapping("/contributions/{username}")
-    public void all(@PathVariable String username) {
+    public List<Week> all(@PathVariable String username) {
+
         Mono<UserContribution> contributionCollections =
-                this.githubGraphQLService.getContributionCollections(username, "2023-05-01T00:00:00Z", "2023-05-31T23:59:00Z");
+                this.githubGraphQLService.getContributionCollections
+                        (username, "2023-05-01T00:00:00Z", "2023-05-31T23:59:00Z");
+        List<Week> weeks = contributionCollections
+                .block()
+                .getContributionsCollection()
+                .getContributionCalendar()
+                .getWeeks();
+        return weeks;
 
-        contributionCollections.subscribe(
-                response -> {
-                    this.weeks.addAll(
-                            response.getContributionsCollection()
-                                    .getContributionCalendar()
-                                    .getWeeks());
 
-                });
-        System.out.println(this.weeks.size());
     }
 
 
