7ab4e5d00aed72dcbd2fc7661611194c5315744c
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-20 00:05:06 +0100

    return message in TelegramService

--- src/main/java/com/zayzou/telegram/TelegramService.java
+++ src/main/java/com/zayzou/telegram/TelegramService.java
@@ -12,6 +12,8 @@ import com.zayzou.utils.OkHttpUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class TelegramService {
 
@@ -36,10 +38,10 @@ public class TelegramService {
 
 
     public String getResult(String command) {
-       /* String message = Objects.equals(command, "today") ? githubService.getTodayContribution() : githubService.getCurrentYearContribution(); // Fetch Github contributions based on command ("today" or "year")
+        String message = Objects.equals(command, "today") ? githubService.getTodayContribution() : githubService.getYearlyContributions(); // Fetch Github contributions based on command ("today" or "year")
         String reaction = message.startsWith("No") ? "😭" : "🥳"; // Set reaction emoji based on Github contributions message
         message = String.format("%s %s", reaction, message); // Format the final message with reaction emoji
-        return message;*/
-        return "";
+        return message;
+
     }
 }
