38f371f978ada49ed1b8066318f69c0409204702
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-05-19 22:44:42 +0100

    replace GithubService with GithubGraphQLService

--- src/main/java/com/zayzou/service/TelegramService.java
+++ src/main/java/com/zayzou/service/TelegramService.java
@@ -7,24 +7,23 @@
 
 package com.zayzou.service;
 
+import com.zayzou.graphql.GithubGraphQLService;
 import com.zayzou.utils.OkHttpUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.util.Objects;
 
 @Service
 public class TelegramService {
 
     private final OkHttpUtils okHttpUtils; // OkHttpUtils instance for making HTTP calls
-    private final GithubService githubService; // GithubService instance for fetching Github contributions data
+    private final GithubGraphQLService githubService; // GithubService instance for fetching Github contributions data
 
     @Value("${telegram.userId}")
     private String userId; // Telegram user ID injected from properties file
     @Value("${telegram.botId}")
     private String botId; // Telegram bot ID injected from properties file
 
-    public TelegramService(OkHttpUtils okHttpUtils, GithubService githubService) {
+    public TelegramService(OkHttpUtils okHttpUtils, GithubGraphQLService githubService) {
         this.okHttpUtils = okHttpUtils;
         this.githubService = githubService;
     }
@@ -37,9 +36,10 @@ public class TelegramService {
 
 
     public String getResult(String command) {
-        String message = Objects.equals(command, "today") ? githubService.getTodayContribution() : githubService.getCurrentYearContribution(); // Fetch Github contributions based on command ("today" or "year")
+       /* String message = Objects.equals(command, "today") ? githubService.getTodayContribution() : githubService.getCurrentYearContribution(); // Fetch Github contributions based on command ("today" or "year")
         String reaction = message.startsWith("No") ? "😭" : "🥳"; // Set reaction emoji based on Github contributions message
         message = String.format("%s %s", reaction, message); // Format the final message with reaction emoji
-        return message;
+        return message;*/
+        return "";
     }
 }
